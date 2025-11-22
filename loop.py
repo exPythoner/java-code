a6e1d5b5b656e2b1ecb37db9932d55574eeb8b1f
exPythoner <1753882026@qq.com>
2025-11-22 14:33:51 +0800

    feat:add loop

---
+++ loop.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# @Time : 2025/11/22 14:27
+# @File : loop.py
+# @Project : git-test-v0
+
+# range()
+for i in range(10):
+    print(f"loop_{i}")
+
+# list 数组
+j = 0
+for name in ['apple', 'python', 'java', 'php', 'redis', 'shell', 'mybatis', 'mybatis-plugin']:
+    print(f"{j}){name}")
+    j = j + 1
