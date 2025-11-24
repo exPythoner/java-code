bb7a920cf0046c737a715ac83340322d4711cd23
exPythoner <1753882026@qq.com>
2025-11-25 02:10:11 +0800

    update: listen trade.order service

---
+++ listenTradeOder.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# @Time : 2025/11/25 02:08
+# @File : listenTradeOder.py
+# @Project : git-test-v1
+
+class ListenTradeOrder:
+    def __init__(self):
+        print("[交易微服务]: 监听订单交易微服务")
